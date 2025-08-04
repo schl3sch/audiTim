47897317948a4a0c555c3997ea3fc0b644e8022b
StmMtn <153722704+StmMtn@users.noreply.github.com>
2025-08-04 19:52:32 +0200

    added notes to docu in txt

--- Frontend/src/app/array-test/array-test.component.spec.ts
+++ Frontend/src/app/array-test/array-test.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ArrayTestComponent } from './array-test.component';
+// import { ArrayTestComponent } from './array-test.component';
 
-describe('ArrayTestComponent', () => {
-  let component: ArrayTestComponent;
-  let fixture: ComponentFixture<ArrayTestComponent>;
+// describe('ArrayTestComponent', () => {
+//   let component: ArrayTestComponent;
+//   let fixture: ComponentFixture<ArrayTestComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ArrayTestComponent]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [ArrayTestComponent]
+//     })
+//     .compileComponents();
 
-    fixture = TestBed.createComponent(ArrayTestComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(ArrayTestComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
