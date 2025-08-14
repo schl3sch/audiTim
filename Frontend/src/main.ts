21ee4749f7da706e8379cf03cb0b436176facc58
Peparani <aaron.reiber2005@gmail.com>
2025-08-15 00:00:11 +0200

    working routing & deleted hash-routing
    fml

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,11 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withHashLocation  } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { App } from './app/app';
 import 'zone.js';
 
 bootstrapApplication(App, {
   providers: [
-    provideRouter(routes, withHashLocation())
+    provideRouter(routes),
+    provideHttpClient() 
   ]
 }).catch(err => console.error(err));
