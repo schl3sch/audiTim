9695159406df85f989317f40bf68846d7aa2d376
Peparani <aaron.reiber2005@gmail.com>
2025-08-18 15:09:31 +0200

    dark mode added

--- Frontend/src/app/navbar/navbar.ts
+++ Frontend/src/app/navbar/navbar.ts
@@ -11,4 +11,26 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class Navbar {
   sidebarCollapsed = false;
+  darkMode = false; // aktueller Zustand
+
+toggleTheme(event: any) {
+  this.darkMode = event.target.checked;
+  const theme = this.darkMode ? 'dark' : 'light';
+
+  // Bootstrap Theme
+  document.documentElement.setAttribute('data-bs-theme', theme);
+
+  // globaler class toggle für Body
+  document.body.classList.remove('light', 'dark');
+  document.body.classList.add(theme);
+
+  localStorage.setItem('theme', theme);
+}
+
+ngOnInit() {
+  const savedTheme = localStorage.getItem('theme') || 'light';
+  this.darkMode = savedTheme === 'dark';
+  document.documentElement.setAttribute('data-bs-theme', savedTheme);
+  document.body.classList.add(savedTheme);
+}
 }
